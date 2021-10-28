356a62ad838578e4d9cc61f5f4ad7c5431030527
louis <louis8ok1@gmail.com>
2021-10-28 22:53:38 +0800

    v0.1
    add FSM

---
+++ HW1.srcs/sources_1/new/geofence.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/10/28 22:25:45
+// Design Name: 
+// Module Name: geofence
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module geofence(
+
+    );
+endmodule
